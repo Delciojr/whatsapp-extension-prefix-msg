aaecb62ece1150efd23da067514a1f613141996f
Delciojr <judelcio14@hotmail.com>
2022-12-07 23:03:49 -0300

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+const input = document.getElementById("nome");
+
+const enviareeeee = (nome) =>{
+    document.execCommand('insertText', false, `*${nome}:* `);
+}
+
+
+form.addEventListener('submit',async (event)=>{
+    event.preventDefault();
+     const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+     chrome.scripting.executeScript({
+        target:{tabId:tab.id},
+        function:enviareeeee,
+        args: [input.value]
+     })
+})
+
